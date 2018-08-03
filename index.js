b12b59d3786d0e87632d2705c42df0d31517d5b8
liyanbin01 <liyanbin01@baidu.com>
2018-08-03 11:26:03 +0800

    debug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import AttractiveForce from './lib/index.js';
 
-const container = document.querySelector('#container');
+const container = document.querySelector('body');
 const opts = {
     count: 100,
     color: '0, 0, 0'
